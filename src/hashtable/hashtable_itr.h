6c9d2d3ad607068bd7ee58953755b9c4419422cb
telehan <wanghansky@gmail.com>
2014-09-11 11:23:22 +0800

    fix inline function not support in C99 mode

--- src/hashtable/hashtable_itr.h
+++ src/hashtable/hashtable_itr.h
@@ -28,20 +28,24 @@ hashtable_iterator(struct hashtable *h);
 /* hashtable_iterator_key
  * - return the value of the (key,value) pair at the current position */
 
-extern inline void *
-hashtable_iterator_key(struct hashtable_itr *i)
-{
-    return i->e->k;
-}
+//extern inline void *
+//hashtable_iterator_key(struct hashtable_itr *i)
+//{
+//    return i->e->k;
+//}
+extern void *
+hashtable_iterator_key(struct hashtable_itr *i);
 
 /*****************************************************************************/
 /* value - return the value of the (key,value) pair at the current position */
 
-extern inline void *
-hashtable_iterator_value(struct hashtable_itr *i)
-{
-    return i->e->v;
-}
+//extern inline void *
+//hashtable_iterator_value(struct hashtable_itr *i)
+//{
+//    return i->e->v;
+//}
+extern void *
+hashtable_iterator_value(struct hashtable_itr *i);
 
 /*****************************************************************************/
 /* advance - advance the iterator to the next element
