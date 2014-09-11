97d3c944633a51eedfc3aa7f52616667887fb0e3
telehan <wanghansky@gmail.com>
2014-09-11 11:12:56 +0800

    fix endian.h and byteswap.h problem

--- src/libcanmdf/mdfsg.c
+++ src/libcanmdf/mdfsg.c
@@ -20,10 +20,28 @@
 
 #include <stdio.h>
 #include <assert.h>
-#include <endian.h>
-#include <byteswap.h>
+//#include <endian.h>
+//#include <byteswap.h>
 #include "mdfsg.h"
 #include "mdfmodel.h"
+
+#ifndef _BYTESWAP_H
+#define _BYTESWAP_H
+//#warning "byteswap.h is an unportable GNU extension! Don't use!"
+static inline unsigned short bswap_16(unsigned short x) {
+ return (x>>8) | (x<<8);
+}
+
+static inline unsigned int bswap_32(unsigned int x) {
+ return (bswap_16(x&0xffff)<<16) | (bswap_16(x>>16));
+}
+
+static inline unsigned long long bswap_64(unsigned long long x) {
+ return (((unsigned long long)bswap_32(x&0xffffffffull))<<32) |
+(bswap_32(x>>32));
+}
+#endif
+#include <machine/endian.h>
 
 /* convert signal to double value */
 double
